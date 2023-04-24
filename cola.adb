3d411d246c90d5c9e47d5b469e0526ce3d4cb348
hachazo <87842940+hachazo@users.noreply.github.com>
2023-04-23 22:34:40 -0300

    update

--- cola.adb
+++ cola.adb
@@ -10,6 +10,31 @@ package body Cola is
    
    end clear;
    
+   --Inserta un elemento en la cola
+   procedure Insert(Item: in Tipodato; Cola: in out Tipocola) is
+   
+   begin
+   
+      if Is_Full(Cola) then 
+	  raise Overflow;
+	  else
+      Cola.Final := Cola.final rem cola.max+1; 
+	  Cola.Elemento(Cola.Final):= item;
+      end if;
+      end insert;
+	  
+   --Elimina el primer elemento de la cola
+   procedure Delete(Cola: in out Tipocola; Item: out tipodato) is
+   
+   begin
+   
+   if Is_empty(Cola) then raise Underflow;
+         else Cola.Frente:= Cola.Frente rem cola.max + 1;
+         item:= cola.elemento(cola.frente);
+      end if;
+	  
+   end Delete;
+	  
    --chequea si la cola esta vacia
    function Is_Empty(Cola: Tipocola) return Boolean is
    
@@ -35,31 +60,6 @@ package body Cola is
    return siguiente=cola.frente;
    
    end Is_Full;
-      
-   --Inserta un elemento en la cola
-   procedure Insert(Item: in Tipodato; Cola: in out Tipocola) is
-   
-   begin
-   
-      if Is_Full(Cola) then 
-	  raise Overflow;
-	  else
-      Cola.Final := Cola.final rem cola.max+1; 
-	  Cola.Elemento(Cola.Final):= item;
-      end if;
-      end insert;
-   
-   --Elimina el primer elemento de la cola
-   procedure Delete(Cola: in out Tipocola; Item: out tipodato) is
-   
-   begin
-   
-   if Is_empty(Cola) then raise Underflow;
-         else Cola.Frente:= Cola.Frente rem cola.max + 1;
-         item:= cola.elemento(cola.frente);
-      end if;
-	  
-   end Delete;
    
    
 end cola;
--- cola.ads
+++ cola.ads
@@ -9,18 +9,18 @@ generic
       
       procedure clear(cola: in out tipocola);
 	  --Limpila los elementos de la cola
+	  
+	  procedure insert(Item : in tipodato; cola : in out tipocola);
+      --Inserta un elemento en la cola
+	  
+	  procedure delete(cola : in out tipocola; Item: out tipodato);
+      --Elimina el primer elemento de la cola
 
       function Is_Empty(cola : tipocola) return Boolean;
       --chaquea si la cola esta vacia
       
       function Is_Full(cola : tipocola) return Boolean;
       --chequea si la cola esta llena
-      
-      procedure insert(Item : in tipodato; cola : in out tipocola);
-      --Inserta un elemento en la cola
-      
-      procedure delete(cola : in out tipocola; Item: out tipodato);
-      --Elimina el primer elemento de la cola
 
       private
       
